a05694bad8672493bcedf2cbb5afc7e434652007
iCarlosAugusto <augusto.dev@proton.me>
2024-07-19 00:10:04 -0300

    loading

--- lib/pages/signin/signin.controller.dart
+++ lib/pages/signin/signin.controller.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ticketapp/components/loadingfullscreen.widget.dart';
 import 'package:ticketapp/pages/home.page.dart';
 
 class SigninController extends GetxController {
 
   final formKey = GlobalKey<FormState>();
 
-  validateForm(){
+  validateForm() async {
     if(formKey.currentState!.validate()){
+      LoadingFullscreen.startLoading();
+      await Future.delayed(const Duration(seconds: 2));
+      LoadingFullscreen.stopLoading();
       Get.to(() => const HomePage());
     }
   }
-
-
 }
